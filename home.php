a371bfc669a5a08bebf6f394395bfb6035f37a27
AlvinChrt <152682677+AlvinChrt@users.noreply.github.com>
2024-07-22 16:20:55 +0700

    Fix

--- home.php
+++ home.php
@@ -52,7 +52,7 @@ include("connect.php");
         <li><a href="room.html">Ruangan</a></li>
         <li><a href="#fiturr">Fasilitas</a></li>
         <li><a href="team.html">Our Team</a></li>
-        <li><a href="logout.php" class="logout">Logout</a></li>
+        <li><a href="index.html" class="logout">Logout</a></li>
       </ul>
     </div>
   </nav>
